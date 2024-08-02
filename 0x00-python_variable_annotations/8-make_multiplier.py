74cc00359fc4079bfcd01c9bed56e961a6fc6029
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-02 04:06:09 +0300

    add: task 8 done

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''
+    Complex types - functions
+'''
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:    
+    """
+    Args:
+        multiplier (float): a float.
+    Returns:
+        a function that multiplies a float by the arg.
+    """
+
+    def func(n: float) -> float:
+        """ multiplies a float by multiplier """
+
+        return float(n * multiplier)
+
+    return func
