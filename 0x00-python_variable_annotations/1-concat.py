664a260cefdda91a964f30953d2cf2850bccdaf6
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-02 03:17:17 +0300

    add: task 1 done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    Basic annotations for variables, concatenate.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two string.
+
+    Args:
+        str1 (string): The first string.
+        str2 (string): The second string.
+
+    Returns:
+        string: The concatenated string.
+    """
+    return str1 + str2
