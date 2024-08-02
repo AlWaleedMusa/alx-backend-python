19b612ba80882fa759d1e546bcc1919281276d10
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-02 03:27:52 +0300

    add: task 3 done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    Basic annotations between float and str.
+'''
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string rep of a float.
+
+    Args:
+        n (float): The number to turn to string.
+
+    Returns:
+        str: The string rep of a floor.
+    """
+
+    return str(n)
