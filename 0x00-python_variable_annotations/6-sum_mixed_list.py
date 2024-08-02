004a704396571c53c51b1f26e2c6a9d4295417b0
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-02 03:51:37 +0300

    fix: changed unior with or oparator

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""
+'''
     Basic annotation 
-"""
-from typing import List, Union
-# Write a type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float.
+'''
+from typing import List
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
     """
     returns the sum of elm in a list as a float.
 
