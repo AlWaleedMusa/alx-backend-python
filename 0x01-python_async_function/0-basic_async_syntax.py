9815530d3f7e5cb55fab3776f324ac664ba58e46
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-06 02:05:54 +0300

    fix: task 0 pip8

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay : int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronously waits for a random delay and returns the delay.
 
