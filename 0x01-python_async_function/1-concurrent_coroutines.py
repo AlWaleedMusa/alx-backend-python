cd461075e30407298b76e639ea8899eb44579820
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-06 02:45:28 +0300

    add: task 1 done

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""execute multiple coroutines"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns wait_random n times with the specified max_delay and returns a list of all the delays in ascending order.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay in seconds for wait_random.
+
+    Returns:
+        List[float]: A list of all the delays in ascending order.
+    """
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
